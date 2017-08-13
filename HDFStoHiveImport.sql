52ceea264b338a3a86d8affd1496f41552648609
waditya <waditya02@gmail.com>
2017-08-13 16:32:05 -0700

    Update HDFStoHiveImport.sql

--- HDFStoHiveImport.sql
+++ HDFStoHiveImport.sql
@@ -21,8 +21,14 @@ load data inpath '/user/cloudera/Transactions.csv' into table transactions;
 
 --Create external tables
 
-create table transactions (AcctNo string, PartNo string, Price decimal(9,2), Qty int)
+create external table moretransactions (AcctNo string, PartNo string, Price decimal(9,2), Qty int)
 row format delimited
 fields terminated by ','
-stored as textfile;
+stored as textfile
+location '/user/cloudera';
 
+
+
+--Load data in external table
+
+load data inpath '/user/cloudera/MoreTransactions.csv' into table moretransactions;
