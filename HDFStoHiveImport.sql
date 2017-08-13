ad54f289afaa234f6b5a460b9559a911e8b4b9e0
waditya <waditya02@gmail.com>
2017-08-13 16:11:59 -0700

    Update HDFStoHiveImport.sql

--- HDFStoHiveImport.sql
+++ HDFStoHiveImport.sql
@@ -1,2 +1,7 @@
 create database rsk;
 use rsk;
+
+create table transactions (AcctNo string, PartNo string, Price decimal(9,2), Qty int)
+row format delimited
+fields terminated by ','
+stored as textfile;
