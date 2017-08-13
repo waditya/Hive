bdac2a55d9c0c02b303b5fda01f69a3178d283e0
waditya <waditya02@gmail.com>
2017-08-13 16:15:47 -0700

    Update HDFStoHiveImport.sql

--- HDFStoHiveImport.sql
+++ HDFStoHiveImport.sql
@@ -1,7 +1,17 @@
+--HiveQL to create a database
+
 create database rsk;
+
+--HiveQL to use a database
 use rsk;
+
+--HiveQL to create data
 
 create table transactions (AcctNo string, PartNo string, Price decimal(9,2), Qty int)
 row format delimited
 fields terminated by ','
 stored as textfile;
+
+-- Query to load data from a file
+
+load data inpath '/user/cloudera/Transactions.csv' into table transactions;
